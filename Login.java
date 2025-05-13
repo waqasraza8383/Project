aae7626e1d6a2b4c116aecb47086c62157144d93
waqasraza8383 <waqasraza1208@gmail.com>
2025-05-13 13:24:13 -0700

    Update Login.java
    i add textfield and button to the Project

--- Login.java
+++ Login.java
@@ -1,34 +1,48 @@
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
-import  java.awt.*;
+import java.awt.*;
 
+class Login extends JFrame {
 
-class Login extends JFrame{
-Login(){
+  JButton rules, back;
+  JTextField tfname;
+
+  Login() {
 
     getContentPane().setBackground(Color.WHITE);
     setLayout(null);
     ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("src/login.jpeg"));
-        JLabel image = new JLabel(i1);
-        image.setBounds(0, 0, 600, 500);
-          add(image);
-           JLabel heading = new JLabel("Simple Minds");
-        heading.setBounds(750, 60, 300, 45);
-        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 40));
-        heading.setForeground(new Color(30, 144, 254));
-        add(heading);
-         JLabel name = new JLabel("Enter your name");
-        name.setBounds(810, 150, 300, 20);
-        name.setFont(new Font("Mongolian Baiti", Font.BOLD, 20));
-        name.setForeground(new Color(30, 144, 254));
-        add(name);
-        setSize(1200, 500);
-        setLocation(200, 150);
-        setVisible(true);
-}
-public static void main(String[] args) {
-  new Login();   
-}
+    JLabel image = new JLabel(i1);
+    image.setBounds(0, 0, 600, 500);
+    add(image);
+    JLabel heading = new JLabel("Simple Minds");
+    heading.setBounds(750, 60, 300, 45);
+    heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 40));
+    heading.setForeground(new Color(30, 144, 254));
+    add(heading);
+    JLabel name = new JLabel("Enter your name");
+    name.setBounds(810, 150, 300, 20);
+    name.setFont(new Font("Mongolian Baiti", Font.BOLD, 20));
+    name.setForeground(new Color(30, 144, 254));
+    add(name);
+    tfname=new JTextField();
+    tfname.setBounds(700, 250, 400, 50);
+    add(tfname);
+    rules=new JButton("Click");
+    rules.setBounds(850,320,100,50);
+    rules.setBackground(new Color(30,144,254));
+    rules.setForeground(Color.white);
+    add(rules);
+    setSize(1200, 500);
+    setLocation(200, 150);
+    setVisible(true);
+  }
+
+  public static void main(String[] args) {
+    new Login();
+  }
 }
